d2d1e7bc356aa7d0a4d629e3a410c5ea7f42d193
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-10-08 19:04:36 +0530

    Create power.java

---
+++ codekatta/power.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+class Digit{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n1=sc.nextInt();
+	    int c=(int)Math.pow(n,n1);
+	    System.out.print(c);
+		
+	}
+}
