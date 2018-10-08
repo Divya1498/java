337ddf2e17403add732cf6135339068952ac91a6
Divya1498 <36623851+Divya1498@users.noreply.github.com>
2018-10-08 18:46:40 +0530

    Create sum.java

---
+++ codekatta/sum.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int K=sc.nextInt();
+		int i,sum=0;
+		int a[]=new int[N];
+		for(i=0;i<N;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<K;i++)
+		{
+			sum=sum+a[i];
+		}
+		System.out.print(sum);
+	}
+}
